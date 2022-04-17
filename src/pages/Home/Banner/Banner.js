bb212342522492f933f8831dba7fb4088f5c8a14
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-04-17 18:16:18 +0600

    Header Part Done

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Banner.css'
+import banner from '../../../image/Banner/banner1.jpg';
 
 const Banner = () => {
 	return (
-		<div>
-			<h2>Banner</h2>
+		<div className='banner'>
+			<img className='w-100'  src={banner} alt="" srcset="" />
+			<div className='banner-title'>
+				<h2>Welcome to Learn Programming</h2>
+				<p>A online plateform Where you can learn programming and grow your programming skill next level</p>
+			</div>
 		</div>
 	);
 };
