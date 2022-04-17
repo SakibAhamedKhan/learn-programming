0e8836b5746beb59fb9bd922f083460d06ccfc6f
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-04-17 16:54:05 +0600

    All files create and installed

---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blog.css'
+
+const Blog = () => {
+	return (
+		<div>
+			<h2>Blog Page</h2>
+		</div>
+	);
+};
+
+export default Blog;
---
+++ src/pages/Shared/Social/Social.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Social.css'
+
+const Social = () => {
+	return (
+		<div>
+			<h2>Social</h2>
+		</div>
+	);
+};
+
+export default Social;
